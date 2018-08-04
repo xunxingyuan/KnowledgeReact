896fe03c57eb943414fb2883379251e1ceccd476
xunxingyuan <xunxingyuan@gmail.com>
2018-08-04 22:45:00 +0800

    add home

--- src/component/home.js
+++ src/component/home.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import api from '../api/api'
 
-const Home = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<h1>Welcome to the U Know</h1>
-		</div>
-	)
+class Home extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            lists: []
+        }
+    }
+	componentWillMount(){
+        api.public.query().then((res) => {
+            if(res.data.code === 200){
+                this.setState({
+                    lists : res.data.data.lists
+                })
+            }
+        }).catch((err) => {
+            
+        });
+    }
+    render() {
+        let listShow = this.state.lists.map((listItem)=>(
+            <div>{listItem.preview}</div>
+        ))
+
+        return (
+            <div className='home'>
+                {listShow}
+            </div>
+        )
+
+    }
 }
 
 export default Home;
--- src/component/info/userInfo.js
+++ src/component/info/userInfo.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import api from '../../api/api'
 
-class userInfo extends React.Component{
-	render(){
+class userInfo extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			userInfo: ''
+		}
+	}
+	componentWillMount() {
+		api.user.getUser().then((res) => {
+			console.log(res)
+			if(res.data.code === 200){
+				this.setState({
+					userInfo: res.data.data
+				})
+			}
+		})
+	}
+	render() {
 		return (
-			<div>{this.props.name}</div>
+			<div className='userInfo'>
+				<div>{this.state.userInfo.name}</div>
+				<div>{this.state.userInfo.email}</div>
+			</div>
+			
 		)
 	}
 }
