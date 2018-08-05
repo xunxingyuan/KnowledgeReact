e7e3c9744c4f753c14f31af159eaa91f834e35fe
xunxingyuan <xunxingyuan@gmail.com>
2018-08-05 22:26:55 +0800

    add home

--- src/component/home.js
+++ src/component/home.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import api from '../api/api'
+import '../styles/home.less'
 
 class Home extends React.Component {
     constructor(){
         super()
         this.state={
-            lists: []
+            lists: [],
+            typeList:[{
+                name:'最新',
+                type:'new'
+            },{
+                name:'我关注的',
+                type:'follow'
+            },{
+                name:'热门',
+                type:'hot'
+            }]
         }
     }
-	componentWillMount(){
+    render() {
+        let listShow = this.state.lists.map((listItem,index)=>(
+            <div key={index}>{listItem.preview}</div>
+        ))
+        let typeMenu = this.state.typeList.map((menu,index)=>(
+            <div className='type' key={index}>{menu.name}</div>
+        ))
+
+        return (
+            <div className='home'>
+                <div className='left'>{listShow}</div>
+                <div className='right'>{typeMenu}</div>
+            </div>
+        )
+
+    }
+    componentDidMount(){
         api.public.query().then((res) => {
             if(res.data.code === 200){
                 this.setState({
@@ -18,18 +45,6 @@ class Home extends React.Component {
         }).catch((err) => {
             
         });
-    }
-    render() {
-        let listShow = this.state.lists.map((listItem)=>(
-            <div>{listItem.preview}</div>
-        ))
-
-        return (
-            <div className='home'>
-                {listShow}
-            </div>
-        )
-
     }
 }
 
